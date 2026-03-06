b7d2b4ab6c471b4a734c4f420d7ec991a73ae073
humble900 <mrcryptowhale8@gmail.com>
2026-03-06 12:08:37 -0800

    fix: wrap BrandKitContent in BrandKitProvider to resolve Next.js build prerender error

--- app/brand-kit/page.tsx
+++ app/brand-kit/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { BrandKitContent } from '@/components/brand-kit/BrandKitContent'
+import { BrandKitProvider } from '@/components/providers/BrandKitProvider'
 
 export default function BrandKitPage() {
   return (
-    <div className="bg-white min-h-screen">
-      <BrandKitContent />
-    </div>
+    <BrandKitProvider>
+      <div className="bg-white min-h-screen">
+        <BrandKitContent />
+      </div>
+    </BrandKitProvider>
   )
 }
